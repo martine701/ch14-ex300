3e4dacfff671f4bb628c7f8db6fcfdfa77c7bf34
Martine Alliet <allietmartine@gmail.com>
2023-07-20 09:23:50 +0100

    commit thursday

---
+++ src/app/customer.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  template: ` <div class="customer">[customer]</div> `,
+  styles: ['.customer {background-color:#fefbd8;margin:10px;padding:10px}'],
+})
+export class CustomerComponent {}
